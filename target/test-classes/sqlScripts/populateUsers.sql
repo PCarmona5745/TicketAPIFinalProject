4b98f1791b9e32ce50d8adcb6171fab7da4c0f00
Peter <108104623+PCarmona5745@users.noreply.github.com>
2022-10-19 22:59:56 -0600

    Delete target directory

--- target/test-classes/sqlScripts/populateUsers.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username) VALUES ('test_man');
-INSERT INTO users (username) VALUES ('IeXist');
-INSERT INTO users (username) VALUES ('nachoLover');
-INSERT INTO users (username) VALUES ('CEOBradley');
-INSERT INTO users (username) VALUES ('bigTester');
